22304bde771e8b8e570a87f67b2b563ceac5eba5
SevenZh7 <z_yu7@u.pacific.edu>
2023-11-20 13:19:01 -0800

    enemy tag, bullet damage, destory when hit

--- Assets/Scripts/BulletTracer.cs
+++ Assets/Scripts/BulletTracer.cs
@@ -7,9 +7,29 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 10f;
     public Rigidbody2D rb;
+    public int damage = 10;
 
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+          /*  Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+            // Apply damage to enemy
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+          */
+            // Destroy the bullet when it hits an object with the "Enemy" tag
+            Destroy(gameObject);
+            Debug.Log("Enemy Hit!");
+        }
     }
 }
