6349b6f8a4c692d7882a0013d29d88285ef3e951
David Yang <d_yang19@u.pacific.edu>
2023-11-26 20:49:25 -0800

    Removed the class 'UnityEditor.ShaderKeywordFilter' in RoomController script

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
