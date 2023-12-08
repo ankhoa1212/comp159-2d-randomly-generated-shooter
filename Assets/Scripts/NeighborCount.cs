f61a4381e625d79c1db49f663b3bf4d03787bb3e
Leah <l_hernandez23@u.pacific.edu>
2023-12-07 22:19:36 -0800

    added anchor to texts and started NeighborCount Script

--- Assets/Scripts/NeighborCount.cs
+++ Assets/Scripts/NeighborCount.cs
@@ -1,9 +1,14 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NeighborCpunt : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,3 +21,4 @@ public class NeighborCpunt : MonoBehaviour
         
     }
 }
+*/
